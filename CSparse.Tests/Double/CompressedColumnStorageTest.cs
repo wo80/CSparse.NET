e384054bb8bb5d521fadf4f226567b788f5b5b3f
cmoha <mselhwala@hotmail.com>
2016-08-21 23:02:37 +0200

    remove unused argument

--- CSparse.Tests/Double/CompressedColumnStorageTest.cs
+++ CSparse.Tests/Double/CompressedColumnStorageTest.cs
@@ -66,7 +66,7 @@ namespace CSparse.Tests.Double
         }
 
         [Test]
-        public void TestJaggedArrayToCSC(int n)
+        public void TestJaggedArrayToCSC()
         {
             double[][] array = new double[3][];
             for (int i = 0; i < 3; i++)
