317d202949120c626b81cc99a4ab6ac0ee7e78ea
wo80 <christian.woltering@tu-dortmund.de>
2016-09-20 16:38:25 +0200

    Add IProgress interface.

---
+++ CSparse/IProgress.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CSparse
+{
+    // If the project gets updated to .NET 4.5, this interface can be replaced
+    // with the official System.IProgress<T> interface.
+
+    /// <summary>
+    /// Used to report progress of a factorization.
+    /// </summary>
+    public interface IProgress
+    {
+        /// <summary>
+        /// Reports a progress update.
+        /// </summary>
+        /// <param name="value">The value of the updated progress (from 0.0 to 1.0).</param>
+        void Progress(double value);
+    }
+}
